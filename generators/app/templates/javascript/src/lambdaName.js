296bb4538b0170d0c3ec8e4e14f395ede411d323
chrisowen <chrisowen491@gmail.com>
2021-03-01 22:16:38 +0000

    First commit

--- generators/app/templates/javascript/src/lambdaName.js
+++ generators/app/templates/javascript/src/lambdaName.js
@@ -4,7 +4,11 @@ exports.handler = async function (event, context) {
   return {
     "headers": { 
       "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*"
+      "Access-Control-Allow-Origin": "*",
+      "Cache-Control": "no-cache, no-store, must-revalidate",
+      "Pragma": "no-cache",
+      "Strict-Transport-Security": "max-age=2592000",
+      "X-Content-Type-Options": "nosniff"
     },
     "statusCode": 200,
     "body": JSON.stringify({message: 'hello world'})
